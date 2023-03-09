7bb1779669241c751b7bc71419459a46da4e9035
anastasia <anastasiamhln@gmail.com>
2023-03-09 17:44:43 +0300

    1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,19 @@
-package org.example;
+//package org.example;
+//
+//import lesson6.db.model.CategoriesExample;
+//import org.apache.ibatis.io.Resources;
+//import org.apache.ibatis.session.SqlSessionFactory;
+//import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+//
+//import java.io.IOException;
+//import java.io.InputStream;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
+
 }
+
+
--- src/test/java/lesson6/GetCategoryTest.java
+++ src/test/java/lesson6/GetCategoryTest.java
@@ -1,10 +1,9 @@
-package lesson5;
+package lesson6;
 
 
 import lesson5.dto.GetCategoryResponse;
 import lesson5.servise.CategoryService;
 import lesson5.util.RetrofitUtils;
-
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -20,20 +19,14 @@ import java.io.InputStream;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-
 public class GetCategoryTest {
     static CategoryService categoryService;
-
-
-
-
     @BeforeAll
     static void beforeAll() throws IOException {
         categoryService = RetrofitUtils.getRetrofit().create(CategoryService.class);
     }
 
-
+    lesson6.db.model.CategoriesExample categoriesExample = new lesson6.db.model.CategoriesExample();
 
     @Test
     void getCategoryByIdPositiveTest() throws IOException {
@@ -42,9 +35,8 @@ public class GetCategoryTest {
         assertThat(response.isSuccessful(), CoreMatchers.is(true));
         assertThat(response.body().getId(), equalTo(1));
         assertThat(response.body().getTitle(), equalTo("Food"));
-        response.body().getProducts().forEach(product -> assertThat(product.getCategoryTitle(), equalTo("Food")));
-
-
+        response.body().getProducts().forEach(product ->
+                assertThat(product.getCategoryTitle(), equalTo("Food")));
 
 
     }
